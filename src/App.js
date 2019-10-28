11500feac2c213bf35e9f18ac7e05f7685d67c25
Steven <bthai7437@gmail.com>
2019-10-27 22:33:30 -0400

    added axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Layout from "./containers/Layout/Layout";
+import FrameTable from "./components/FrameTable/FrameTable";
 import "./App.module.css";
 
 class App extends Component {
   render() {
-    return <div> This is a test - Dave Hughes</div>;
+    return <FrameTable />;
   }
 }
 
