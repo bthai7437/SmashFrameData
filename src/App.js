410dd7991a03961762a6ba5b8ce68f6135ee9c4e
bthai7437 <bthai7437@gmail.com>
2019-11-04 22:20:04 -0500

    added Character selection animation, added new character component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import Layout from "./containers/Layout/Layout";
 import FrameTable from "./components/FrameTable/FrameTable";
+import CharacterSelect from "./containers/Layout/CharacterList/CharacterList";
 import "./App.module.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Layout />
+        <Layout>
+          <CharacterSelect />
+        </Layout>
       </div>
     );
   }
--- src/containers/Layout/CharacterList/CharacterList.js
+++ src/containers/Layout/CharacterList/CharacterList.js
@@ -1,33 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
-import ImageStyle from "./CharacterList.module.css";
+import Character from "./Character/Character";
 
-const CharacterList = props => {
-  console.log(props.characters);
-  if (props.characters[0]) {
-    const images = props.characters.map(image => {
+class CharacterList extends Component {
+  render() {
+    if (this.props.characters[0]) {
+      const images = this.props.characters.map(image => {
+        return (
+          <Col key={image.InstanceId} xs="auto" md="auto">
+            <Character name={image.Name} ThumbnailUrl={image.ThumbnailUrl} />
+          </Col>
+        );
+      });
+
       return (
-        <Col key={image.InstanceId} xs="auto" md="auto">
-          <Image
-            className={ImageStyle.Image}
-            rounded
-            onClick={() => console.log(image.Name)}
-            alt={image.name}
-            src={image.ThumbnailUrl}
-          />
-        </Col>
+        <div>
+          <Row>{images}</Row>
+        </div>
       );
-    });
-
-    return (
-      <div>
-        <Row>{images}</Row>
-      </div>
-    );
+    }
+    return <div>Loading</div>;
   }
-  return <div>Loading</div>;
-};
+}
 
 export default CharacterList;
